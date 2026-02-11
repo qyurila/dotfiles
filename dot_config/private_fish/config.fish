5820d31331e054cf0395bda7ecabf66837083cdd
QriLa <rea9lizer@gmail.com>
2026-02-11 13:28:51 +0900

    Add zellij config

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,10 +1,11 @@
 source ~/.config/fish/abbr.fish
 source ~/.config/fish/envs.fish
 
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
-
 mise activate fish | source
 starship init fish | source
 zoxide init fish | source
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    eval (zellij setup --generate-auto-start fish | string collect)
+end
