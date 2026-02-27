217a5b3fb981d7801b816d8725a2c8f61c55fce1
QriLa <rea9lizer@gmail.com>
2026-02-27 17:54:41 +0900

    Update fish config

--- dot_config/private_fish/abbr.fish
+++ dot_config/private_fish/abbr.fish
@@ -2,4 +2,7 @@ abbr -a -- cd z
 abbr -a -- cm chezmoi
 abbr -a -- lg lazygit
 
+abbr -a -- cma 'chezmoi add'
+abbr -a -- cmg 'lazygit -p ~/.local/share/chezmoi'
+abbr -a -- cmz 'z ~/.local/share/chezmoi'
 abbr -a -- mug 'mise u -g'
--- dot_config/private_fish/envs.fish
+++ dot_config/private_fish/envs.fish
@@ -1,5 +1,7 @@
 set -gx EDITOR hx
 
+set -gx fzf_fd_opts --hidden
+
 # Use BitWarden SSH on WSL
 if test -f /proc/sys/fs/binfmt_misc/WSLInterop
     set -gx GIT_SSH_COMMAND 'ssh.exe -F ~/.ssh/config'
